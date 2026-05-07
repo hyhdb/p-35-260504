8314900ccabc73340c7b7d032b468a65a7d5731b
hyhdb <kkminwoo@gmail.com>
2026-05-07 11:02:18 +0900

    31

--- backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
+++ backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
@@ -19,12 +19,13 @@ class ApiV1AdmMemberController(
         @RequestParam("page", defaultValue = "1") page: Int,
         @RequestParam("pageSize", defaultValue = "2") pageSize: Int,
         @RequestParam("kw", defaultValue = "") kw: String,
+        @RequestParam("kwType", defaultValue = "ALL") kwType: String,
     ): PageDto<MemberWithUsernameDto> {
 
         val page = if (page >= 1) page else 1
         val pageSize = if (pageSize >= 5) pageSize else 5
 
-        val pagedResult = memberService.findByPaged(page, pageSize, kw)
+        val pagedResult = memberService.findByPaged(page, pageSize, kw, kwType)
 
         return PageDto(
             pagedResult.map {
